88a1d73f8c8a80496e789c7b197779c24d4b630c
osm177 <ali.ahmed.osman@student.laurea.fi>
2026-09-17 11:53:35 +0300

    js

--- WS02_JavaScript_Basics/js/script.js
+++ WS02_JavaScript_Basics/js/script.js
@@ -6,35 +6,48 @@ console.log("Hello, World!");
 
 // Store text values in variables
 
-const userName = "Juho";
-const favoriteAnimal = "Blue Elephant";
+
+//const favoriteAnimal = "Blue Elephant";
+
+const userName = "Ali";
+const favoriteSport = "Jalkapallo";
 
 
 // Print the variables to the console
-console.log("My name is " + userName);
-console.log("My favorite animal is " + favoriteAnimal);
+console.log("My name is " + userName );
+console.log("My favoriteSport is " + favoriteSport);
 // STEP 3: User interaction
 // Display a pop-up message
-alert("tervertetuloa hieoulle nettisivylleni!");
+
+alert("Tervetuloa minun hienoille nettisivuilleni!");
 
 // Ask the user for their name
 
-const visitorName = prompt("Mikä on nimesi?");
+const visitorName = prompt("Mikä on nimesi ?");
+//const visitorName = prompt("Mikä on nimesi?");
 
 // Print the user's answer
 console.log(visitorName);
 
-console.log("Terve " + visitorName + "! Tervetuloa javasciptimn psariin!");
-// Ask the user for their favorite animal
 
-const visitorFavoriteAnimal = prompt("Mikä on lempieläimesi?");
+console.log("Terve " + visitorName + "! Tervetuloa javasciptin pariin!");
+// Ask the user for their favorite Sport
+
+const visitorFavoriteSport = prompt("Mikä on lempiurheilusi?");
 
 // Create a sentence using both answers
 
-console.log("Hello, " + visitorName + "! Your favorite animal is " + visitorFavoriteAnimal + ".");
+console.log("Hello, " + visitorName + "! Your favorite sport is " + visitorFavoriteSport + ".");
 
 // ⭐⭐ BONUS Ask the user for their favorite animal.If their favorite animal is "Blue Elephant", print: Great choice! That's my favorite animal too!
 // ⭐⭐ BONUS Otherwise, print: Nice! Your favorite animal is [animal]. Can you figure out how to do this using if / else?
+const lempiUrheilusi = prompt("Mikä on lempiurheilusi?");
+
+if (lempiUrheilusi === "Jalkapalo") {
+    console.log("Great choice!");
+} else {
+    console.log("Kivaa! Sinun lempiurheilusi on " + lempiUrheilusi + " .");
+}
 
 // const visitorFavoriteAnimal = prompt("What is your favorite animal?");
 
